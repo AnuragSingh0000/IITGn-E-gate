61239658a6d7740c992fa7098e28970241bd0bdb
adi776borate <166858869+adi776borate@users.noreply.github.com>
2024-04-14 14:22:04 +0530

    Update Dump20240414.sql

--- Dump20240414.sql
+++ Dump20240414.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `email`
 --
-
+USE iitgn;
 DROP TABLE IF EXISTS `email`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
